c8a264e86e86061712cdf660fd46674464f3f159
defanor <defanor@uberspace.net>
2016-10-05 07:37:00 +0300

    Handle relative URIs

--- prototype/Feed.hs
+++ prototype/Feed.hs
@@ -17,6 +17,7 @@ import Control.Monad
 import Control.Applicative
 import Data.List
 import Data.Time.Clock
+import Network.URI
 
 import Types
 
@@ -33,14 +34,14 @@ getFeed :: FilePath -> String -> IO [Maybe (UTCTime, Block)]
 getFeed cd uri = do
   r <- httpLBS =<< parseRequest uri
   case parseFeedString (L8.unpack $ responseBody r) of
-    Just f -> mapConcurrently (parseItem cd) (getFeedItems f)
+    Just f -> mapConcurrently (parseItem uri cd) (getFeedItems f)
     Nothing -> pure []
 
-parseItem :: FilePath -> Item -> IO (Maybe (UTCTime, Block))
-parseItem cd i = do
+parseItem :: String -> FilePath -> Item -> IO (Maybe (UTCTime, Block))
+parseItem uri cd i = do
   case (getItemPublishDate i, getItemSummary i <|> getItemDescription i) of
     (Just (Just t), Just sum) -> do
-      summary <- extractBlocks' cd $ parseTags sum
+      summary <- extractBlocks uri cd $ parseTags sum
       let blocks = concat
             [[BParagraph [IText $ "Date: " ++ show t]],
              summary,
@@ -56,25 +57,30 @@ showTC other = show other
 makeURI :: Link -> Inline
 makeURI l = ILink (maybe (linkHref l) id (linkTitle l)) (linkHref l)
 
-extractBlocks :: FilePath -> TextContent -> IO [Block]
-extractBlocks cd (HTMLString s) = extractBlocks' cd $ parseTags s
-extractBlocks _ other = pure [BParagraph [IText $ showTC other]]
+imageURI :: String -> [Attribute String] -> Maybe String
+imageURI uri img = do
+  src <- lookup "src" img
+  if isRelativeReference src
+    then do
+    src' <- parseRelativeReference src
+    uri' <- parseAbsoluteURI uri
+    pure . show $ src' `relativeTo` uri'
+    else pure src
 
-extractBlocks' :: FilePath -> [Tag String] -> IO [Block]
-extractBlocks' _ [] = pure []
-extractBlocks' cd (TagOpen "img" img : xs) =
-  case lookup "src" img of
-    Nothing -> pure []
-    Just uri -> do
-      let fn = cd </> (fromJust $ lookup "src" img)
+extractBlocks :: String -> FilePath -> [Tag String] -> IO [Block]
+extractBlocks _ _ [] = pure []
+extractBlocks uri cd (TagOpen "img" img : xs) =
+  case imageURI uri img of
+    Just absURI -> do
+      let fn = cd </> absURI
           picture = Just $ BImage fn
           alt = lookup "alt" img >>= \a -> pure (BParagraph [IText a])
       exists <- doesFileExist fn
       unless exists $ do
         createDirectoryIfMissing True (takeDirectory fn)
-        r <- httpLBS =<< parseRequest uri
+        r <- httpLBS =<< parseRequest absURI
         L8.writeFile fn $ responseBody r
-      rest <- extractBlocks' cd xs
+      rest <- extractBlocks uri cd xs
       pure $ catMaybes [picture, alt] ++ rest
-extractBlocks' cd (TagText txt : xs) = (BParagraph [IText txt] :) <$> extractBlocks' cd xs
-extractBlocks' cd (_:xs) = extractBlocks' cd xs
+extractBlocks uri cd (TagText txt : xs) = (BParagraph [IText txt] :) <$> extractBlocks uri cd xs
+extractBlocks uri cd (_:xs) = extractBlocks uri cd xs
